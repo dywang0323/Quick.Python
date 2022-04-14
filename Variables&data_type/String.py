05ac494b40df3e4abc9caa31740c307b134e3e13
Dongyu Wang <60108209+dywang0323@users.noreply.github.com>
2022-04-14 01:33:56 -0500

    Update String.py

--- Variables&data_type/String.py
+++ Variables&data_type/String.py
@@ -1,1 +1,22 @@
+# change the case of a string
+# capitalize the first letter
 
+name = "my lovelace"
+print(name.title())
+
+# capitalize all the letter, lowercase all letters, case conversion
+name = "my lovelace"
+print(name.upper())
+print(name.lower())
+print(name.swapcase())
+
+# merge strings
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+print(full_name)
+print("hello, " + full_name.title() + "!")
+
+message = "hello, " + full_name.title() + "!"
+print(message)
