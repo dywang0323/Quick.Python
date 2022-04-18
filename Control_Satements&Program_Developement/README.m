8a707aa61ff3d82e64483867efd2b4d2b47286d7
Dongyu Wang <60108209+dywang0323@users.noreply.github.com>
2022-04-18 17:23:41 -0500

    Update and rename README to README.m

---
+++ Control_Satements&Program_Developement/README.m
@@ -0,0 +1,5 @@
+# Control statements
+
+Usually, statements in a program excute in the order in which they're written, this is called 'sequential execution'. Various python statements enable you
+to specify that the next statement to excute may be other than the next one in sequence. this is called transfer of control and is achieved with python 
+control statements
